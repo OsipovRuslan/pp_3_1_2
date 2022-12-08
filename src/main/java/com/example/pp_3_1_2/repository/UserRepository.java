4d7e94ac9a4b6e28146c346c652f0a246db74469
Doven1Kanobe <ruslanosipov3524@yandex.ru>
2022-12-08 13:59:15 +0400

    qwe

--- src/main/java/com/example/pp_3_1_2/repository/UserRepository.java
+++ src/main/java/com/example/pp_3_1_2/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.example.pp_3_1_2.repository;
 
 import com.example.pp_3_1_2.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
